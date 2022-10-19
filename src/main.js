1306530b39823827b6d55f8a84d17f002b0aa650
Marcelo Carvalho <marcelo_scarvalho@outlook.com>
2022-10-19 12:47:39 -0300

    Imask com expressão regular

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "./css/index.css"
+import IMask from "imask"
 
+// Mudança de estilo com setAttribute
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
@@ -22,15 +24,89 @@ function setCardType(type) {
 }
 globalThis.setCardType = setCardType
 
-const cardNumber = document.querySelector("#card-number")
-cardNumber.addEventListener("input", updateValue)
+// CVC
+var inputSecurityCode = document.querySelector("#security-code")
+var securityCodePattern = {
+  mask: "0000",
+}
+var securityMasked = IMask(inputSecurityCode, securityCodePattern)
 
-function updateValue() {
-  if (cardNumber.value === "50" || "40") {
-    setCardType("visa")
-  } else if (cardNumber.value === "58") {
-    setCardType("mastercard")
-  } else {
-    setCardType("default")
+// DATA EXP
+var inputDateExp = document.querySelector("#expiration-date")
+var dateExpPattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: new String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
   }
 }
+var dateExpMasked = IMask(inputDateExp, dateExpPattern)
+
+// NUMBER CARD
+var inputNumberCard = document.querySelector("#card-number")
+var cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(5[1-5]\d{0,2}|^22[2-9]\d{0,1}|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 000000 00000",
+      regex: /^3[47]\d{0,13}/,
+      cardtype: "american express",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: "maestro",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardtype: "discover",
+    },
+    {
+      mask: "0000 000000 0000",
+      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+      cardtype: "diners",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardtype: "jcb",
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^62\d{0,14}/,
+        cardtype: 'unionpay'
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    // Pegando somente os numeros digitados, troca "NÃO DIGITO" por vazio
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+    console.log(foundMask.cardtype)
+    return foundMask
+  },
+}
+var cardNumberMasked = IMask(inputNumberCard, cardNumberPattern)
